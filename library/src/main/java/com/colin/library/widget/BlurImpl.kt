c113702ae417221a9c20e4acfd03df66636531cc
Colin_default <15910672817@163.com>
2020-08-07 10:30:48 +0800

    Initial commit

---
+++ library/src/main/java/com/colin/library/widget/BlurImpl.kt
@@ -0,0 +1,11 @@
+package com.colin.library.widget
+
+import android.content.Context
+import android.graphics.Bitmap
+
+internal interface BlurImpl {
+    fun prepare(context: Context?, buffer: Bitmap?, radius: Float): Boolean
+
+    fun release()
+    fun blur(input: Bitmap?, output: Bitmap?)
+}
